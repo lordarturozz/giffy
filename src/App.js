b23cbbb713fa7cbbcfa40d8fc5142b6288c0205a
lordarturozz <lordarturozz@gmail.com>
2024-06-27 15:16:54 -0600

    Nuevo update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import {useEffect, useState } from 'react';
 import './App.css';
 
+const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=rBM8M99200b9OF1ukOdPKaFVBwM6jvM2&q=Michael+Jackson&limit=10&offset=0&rating=g&lang=en&bundle=clips_grid_picker';
+const url = 'https://api.giphy.com/v1/gifs/search?api_key=rBM8M99200b9OF1ukOdPKaFVBwM6jvM2&q=Michael+Jackson&limit=10&offset=0&rating=g&lang=en&bundle=clips_grid_picker';
 function App() {
+  const [gifs,setGifs] = useState([]);
+
+  useEffect(function (){
+   console.log('Actualizando los gifs');
+   fetch(apiURL)
+    .then(res => res.json())
+    .then(response => {
+      const {data} = response
+      const gifs = data.map(image => image.fixed_width.url);
+      setGifs(gifs);
+    })
+    }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='App-content'>
+        {
+          gifs.map(singleGif  => <img src= {singleGif} />)
+        }
+       
+      </section>
     </div>
   );
 }
