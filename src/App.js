9164eed06e43e1b6e8b99a2c26f44c34eabcbe21
lordarturozz <lordarturozz@gmail.com>
2024-06-28 18:21:39 -0600

    Nuevo Update 28/06/24

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ export default function App(){
     <div className="App">
       <section className="App-content">
        <h1>App</h1>
-       <a href='/Gif/MJ'></a>
+       <a href='/Gif/MJ'>Gif de Michael Jackson</a>
+       <a href='/Gif/Spiderman'>Gif de Spiderman</a>
+       <a href='/Gif/GOT'>Gif de Game Of Thrones</a>
        
       <Route component={ListOfGifs} 
       path="/gif/:keyword"  />
